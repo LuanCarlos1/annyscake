24ca9dbab232ce82914650a81e8216b41ab67bbb
Luan Carlos <luan.k28@hotmail.com>
2025-05-29 16:45:01 -0300

    Criado a tela de status do usuario

---
+++ app/src/main/java/br/com/example/annyscake/TelaUsuario.java
@@ -0,0 +1,32 @@
+package br.com.example.annyscake;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Objects;
+
+public class TelaUsuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_tela_usuario);
+
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
